ace2f38a3cfc069fb6a23b20717ae17beb03882d
Hlib Chibichian <chibgleb99@gmail.com>
2023-11-17 11:53:48 +0000

    7th task

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+        LogParser logParser = new LogParser(Paths.get("C:\\Users\\My PC\\IdeaProjects\\untitled5\\src\\example.log"));
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
     }
 }
